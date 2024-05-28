afcb5ff614f28c93219e4eda7635b1713cee5527
Andikarya17 <andikaryaputra17@gmail.com>
2024-05-28 15:44:47 +0700

    membuat enkapsulasi

---
+++ enkapsulasi.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class remotelampu {
+private:
+	string saklarNo[10];
+
+public:
+	void setsaklarNo(int i, string value)
+	{
+		saklarNo[i] = value;
+	}
+	string getsaklarNo(int i)
+	{
+		return saklarNo[i];
+	}
+};
+int main() {
+	remotelampu lampuRumah;
+
+	lampuRumah.setsaklarNo(0, "Lampu Teras Rummah");
+	lampuRumah.setsaklarNo(1, "Lampu Ruang Tamu");
+	lampuRumah.setsaklarNo(2, "Lampu kamar Tidur");
+	lampuRumah.setsaklarNo(3, "Lampu dapur ");
+
+	cout << lampuRumah.getsaklarNo(0) << endl;
+	cout << lampuRumah.getsaklarNo(1) << endl;
+	cout << lampuRumah.getsaklarNo(2) << endl;
+	cout << lampuRumah.getsaklarNo(3) << endl;
+
+	return 0;
+
+}
